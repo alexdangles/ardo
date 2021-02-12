26c2b1dfb0a937ca909b9daaba80bc32031b9586
Alex D'Angelo <alexdangles@gmail.com>
2021-02-12 12:26:11 -0800

    .

--- serial_coms/serial_coms.ino
+++ serial_coms/serial_coms.ino
@@ -1,11 +1,13 @@
-int led = 2; //pin number of output
+int led = 2; //pin number of led
+int laser=3;//pin number of laser
 bool pulse = false; //led pulsing state
 int pulseRate = 200; //rate between LED pulses (ms)
 
 void setup(){
   pinMode(led, OUTPUT); //digital pin 2 as output
+  pinMode(laser, OUTPUT); //digital pin 3 as output
   digitalWrite(led, HIGH); //LED on at init
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop(){
@@ -18,13 +20,15 @@ void loop(){
   if(Serial.available() > 0){
     pulse = false;
     String msg = Serial.readString();
-    if(msg == "on"){ //turn on LEDs
-      digitalWrite(led, HIGH);
-      Serial.println("LEDs are now on");
+    if(msg == "led"){ //toggle LEDs
+      int state =  !digitalRead(led);
+      msg = state? "LEDs are on" : "LEDs are off";
+      digitalWrite(led, state);
     }
-    else if(msg == "off"){ //turn off LEDs
-      digitalWrite(led, LOW);
-      Serial.println("LEDs are now off");
+    else if(msg == "las"){ //toggle laser
+      int state =  !digitalRead(led);
+      msg = state? "Laser is on" : "Laser is off";
+      digitalWrite(laser, state);
     }
     else if(msg == "pulse"){ //pulse LEDs
       pulse = true;
@@ -33,5 +37,6 @@ void loop(){
     else{
       Serial.println("not a valid command");
     }
+    Serial.println(msg);
   }
 }
