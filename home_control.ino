79c0d2407fcb79bf68de98c84ecb2c251430b159
Alex D'Angelo <alexdangles@gmail.com>
2021-04-21 15:13:13 -0700

    updated status cmd

--- home_control.ino
+++ home_control.ino
@@ -1,65 +1,143 @@
 bool stringComplete = false;
 String msg = "";
 
-int led = 2; //pin number of led
-int laser = 3;//pin number of laser
-bool pulse = false; //led pulsing state
-int pulseRate = 200; //rate between LED pulses (ms)
+int LED = 2;                       // pin number of LED
+bool ledState = true;              // LED init state
+int LASER = 3;                     // pin number of LASER
+bool laserState = false;           // laser init state
+bool pulse = false;                // LED pulsing state
+bool tog = true;                   // toggle switch for LEDs
+int ledPulseRate_ms = 100;         // rate between LED pulses (ms)
+unsigned long ledToggle_min = 720; // toggle LEDs every x minutes
+unsigned long previousMillis = 0;  // millis() returns an unsigned long.
 
-void setup(){
-  pinMode(led, OUTPUT); //digital pin 2 as output
-  pinMode(laser, OUTPUT); //digital pin 3 as output
-  digitalWrite(led, HIGH); //LED on at init
-  Serial.begin(115200); //connect to serial port
+void setup()
+{
+  pinMode(LED, OUTPUT);            // digital pin 2 as output
+  pinMode(LASER, OUTPUT);          // digital pin 3 as output
+  Serial.begin(115200);            // connect to serial port
+  digitalWrite(LED, ledState);     // LED state at init
+  digitalWrite(LASER, laserState); // Laser state at init
 }
 
-void loop(){
-  if(pulse){ //pulse condition
-    digitalWrite(led, HIGH);
-    delay(pulseRate);
-    digitalWrite(led, LOW);
-    delay(pulseRate);
+void loop()
+{
+  // Schedule LEDs on/off
+  unsigned long currentMillis = millis(); // grab current time
+  // check if "interval" time has passed (1000 milliseconds)
+  if ((unsigned long)(currentMillis - previousMillis) >= (ledToggle_min * 60 * 1000))
+  {
+    tog = !tog;                // "toggles" the state
+    digitalWrite(LED, tog);    // sets the LED based on toggle
+    previousMillis = millis(); // save the "current" time
   }
-  if (stringComplete){
 
-      stringComplete = false;
-      msg.trim();
+  if (pulse)
+  { // pulse condition
+    digitalWrite(LED, HIGH);
+    delay(ledPulseRate_ms);
+    digitalWrite(LED, LOW);
+    delay(ledPulseRate_ms);
+  }
+  if (stringComplete)
+  {
 
-      if(msg == "leds"){ //toggle LEDs
+    stringComplete = false;
+
+    if (msg.indexOf("leds") >= 0)
+    { // toggle LEDs
       pulse = false;
-      int state =  !digitalRead(led);
-      msg = state? "LEDs are on" : "LEDs are off";
+      ledState = !digitalRead(LED);
+      digitalWrite(LED, ledState);
+      msg = ledState ? "on" : "off";
+      Serial.print("LEDs are ");
       Serial.println(msg);
-      digitalWrite(led, state);
-      }
-      else if(msg == "laser"){ //toggle laser
-        int state =  !digitalRead(laser);
-        msg = state? "Laser is on" : "Laser is off";
-        Serial.println(msg);
-        digitalWrite(laser, state);
-      }
-      else if(msg == "pulse"){ //pulse LEDs
-        pulse = true;
-        Serial.println("LEDs are pulsing");
-      }
-      else{
-        Serial.println("not a valid command");
-      }
     }
-    msg = "";
-    delay(500);
+    else if (msg.indexOf("laser") >= 0)
+    { // toggle LASER
+      laserState = !digitalRead(LASER);
+      digitalWrite(LASER, laserState);
+      msg = laserState ? "on" : "off";
+      Serial.print("Laser is ");
+      Serial.println(msg);
+    }
+    else if (msg.indexOf("pulse") >= 0)
+    { // pulse LEDs
+      char rate = msg.charAt(5);
+      switch (rate)
+      {
+      case '1':
+        ledPulseRate_ms = 20;
+        pulse = true;
+        break;
+      case '2':
+        ledPulseRate_ms = 50;
+        pulse = true;
+        break;
+      case '3':
+        ledPulseRate_ms = 200;
+        pulse = true;
+        break;
+      case '4':
+        ledPulseRate_ms = 500;
+        pulse = true;
+        break;
+      case '5':
+        ledPulseRate_ms = 1000;
+        pulse = true;
+        break;
+      default:
+        ledPulseRate_ms = 100;
+        pulse = false;
+        break;
+      }
+      msg = pulse ? "pulsing" : "off";
+      Serial.print("LEDs are ");
+      Serial.println(msg);
+    }
+    else if (msg.indexOf("status") >= 0)
+    { // status info
+      Serial.print("timer counts (ms): ");
+      Serial.println(currentMillis, DEC);
+      msg = tog ? "off" : "on";
+      int timeLeft = ledToggle_min - ((currentMillis - previousMillis) / 1000 / 60);
+      int hours = timeLeft / 60;
+      int min = timeLeft % 60;
+      Serial.print("LEDs " + msg + " in ");
+      Serial.print(hours, DEC);
+      Serial.print(" hours and ");
+      Serial.print(min, DEC);
+      Serial.println(" min");
+      Serial.print("pulse rate (ms): ");
+      Serial.println(ledPulseRate_ms, DEC);
+      msg = ledState ? "on" : "off";
+      Serial.println("LEDs are " + msg);
+      msg = laserState ? "on" : "off";
+      Serial.println("Laser is " + msg);
+    }
+    else
+    {
+      Serial.println("not a valid command");
+    }
+  }
+  msg = "";
+  delay(50);
 }
 
-void serialEvent(){//receives msg from user input
-  while (Serial.available()){
+void serialEvent()
+{ // receives msg from user input
+  while (Serial.available())
+  {
     // get the new byte:
     char inChar = (char)Serial.read();
     // add it to the inputString:
     msg += inChar;
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
-    if (inChar == '\n'){
+    if (inChar == '\n')
+    {
       stringComplete = true;
+      msg.trim();
     }
   }
 }
